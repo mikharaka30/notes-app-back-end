ed51de3824e09f9717c6fd206fd97951eac6d5f6
Michael Rakavarrel (Varrel) <mikharaka30@gmail.com>
2023-09-27 16:49:48 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,12 @@
+const notes = [
+    // {
+    //     id: string,
+    //     title: string,
+    //     createdAt: string,
+    //     updateAt: string,
+    //     tags: array of string,
+    //     body: string,
+    // },
+];
+
+module.exports = notes;
